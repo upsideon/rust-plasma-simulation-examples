f654896b1ee83eb1014ce16d9f6c7c9fc66323c6
upsideon <65986739+upsideon@users.noreply.github.com>
2022-07-31 22:47:20 -0600

    Adding initial code for grounded box plasma simulation.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,16 @@
 FROM rust:1.62-alpine
 
 WORKDIR /usr/src/plasma-simulation
+
+COPY ./Cargo.toml .
+COPY ./Cargo.lock .
+
+RUN mkdir ./src \
+    && echo 'fn main() { println!("Build for dependency caching."); }' > src/main.rs \
+    && cargo build --release
+
+RUN rm -rf ./src ./target
+
 COPY . .
 
 RUN cargo install --path .
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+mod grounded_box;
 mod single_particle;
 
 fn main() -> std::io::Result<()> {
     const NUM_MESH_NODES: usize = 21;
 
     println!("Rust Plasma Physics Simulation");
+
+    println!("Running single particle simulation...");
     single_particle::simulate(NUM_MESH_NODES)?;
+    println!("Single particle simulation complete.");
+
+    println!("Running grounded box multi-particle simulation...");
+    grounded_box::simulate(NUM_MESH_NODES)?;
+    println!("Grounded box multi-particle simulation complete.");
 
     Ok(())
 }
