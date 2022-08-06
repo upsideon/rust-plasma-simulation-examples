87b4ec992f8917cdb8b0daeb30eee13c13382e3d
upsideon <65986739+upsideon@users.noreply.github.com>
2022-08-06 10:23:54 -0600

    Adding timing code for simulations.

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,25 @@ mod vector;
 mod grounded_box;
 mod single_particle;
 
+use std::time::Instant;
+
 fn main() -> std::io::Result<()> {
     const NUM_MESH_NODES: usize = 21;
 
     println!("Rust Plasma Physics Simulation");
 
     println!("Running single particle simulation...");
+    let now = Instant::now();
     single_particle::simulate(NUM_MESH_NODES)?;
+    let elapsed_time = now.elapsed();
+    println!("Simulation took {} seconds.", elapsed_time.as_secs());
     println!("Single particle simulation complete.");
 
     println!("Running grounded box multi-particle simulation...");
+    let now = Instant::now();
     grounded_box::simulate(NUM_MESH_NODES)?;
+    let elapsed_time = now.elapsed();
+    println!("Simulation took {} seconds.", elapsed_time.as_secs());
     println!("Grounded box multi-particle simulation complete.");
 
     Ok(())
